d37e75aae0bf9e504f25ed1fc2fd690da7b096ac
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-09-27 01:15:15 +0600

    Arethmetic operation

--- main.dart
+++ main.dart
@@ -1,9 +1,26 @@
 void main() {
   // Type safety
   String name = "Adib";
+
   // Unsafe
   var name2 = "Adib from unsafe";
-  
+
+  // adding teo string
+  var fname = "Mohammed";
+  var lname = "ADIB";
+
+  //mathematical operations
+  int a = 12;
+  int b = 23;
+
+  //print the mathes result
+
+  print(a + b);
+  print(a * b);
+  print(a / b);
+  print(a - b);
+
+  print(fname +" "+ lname);
   print(name);
   print(name2);
 }
