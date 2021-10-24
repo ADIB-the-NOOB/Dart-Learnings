4a3fc26b58ed3f50b6d7462fc75d1f11e6139f2e
Mohammed Adib <adib@adib.adib>
2021-10-24 19:25:59 +0600

    String & math

---
+++ math.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+void main() {
+  // prnt minimum value
+  print(min(1211, 1231));
+
+  // prnt maximum value
+  print(max(2131313, 12212));
+
+  // print square root of a number
+  print(sqrt(169));
+}
---
+++ tutorial2.dart
@@ -0,0 +1,24 @@
+void main() {
+  //boolean
+  bool isMale = false;
+  bool isFemale = true;
+
+  print(isFemale);
+  print(isMale);
+
+  //string related
+  String am = "Hello world! in dart ";
+  print(am.length);
+  print(am.toUpperCase());
+  print(am.toLowerCase());
+
+  //string searching
+  String a = "not a Great Coder, But want to be";
+  print(a.contains("a"));
+  print(a.contains("z"));
+  print(a.contains("x"));
+
+  //boolean advanced
+  print(5 == 5);
+  print(5 == 4);
+}
